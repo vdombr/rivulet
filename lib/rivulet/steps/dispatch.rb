2f7c80938d1f2a19cd7b55dc7a8e8eb99cc25d98
Vladimir Dombrovskiy <vold@fastmail.com>
2026-06-23 22:32:05 +0200

    Add spec for Dispatch step

--- lib/rivulet/steps/dispatch.rb
+++ lib/rivulet/steps/dispatch.rb
@@ -2,7 +2,9 @@ module Rivulet
   module Steps
     class Dispatch < Rivulet::Step
       def call(input)
-        result = input[:route].callable.call(input.slice(:params, :context))
+        input => { route:, params:, context: }
+
+        result = route.callable.call(params: params, context: context)
 
         # We don't care if it's success or failure.
         # The result will be a response anyway.
